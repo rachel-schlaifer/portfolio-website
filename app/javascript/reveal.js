a02f74cefdfc91693eb621a6f13fb0ff34ad3af5
Rachel Schlaifer <rachelschlaifer9@gmail.com>
2026-08-02 15:06:18 +0900

    feat: V2 of portfolio website

--- app/javascript/reveal.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const arrow = document.querySelector('.big-arrow');
-  const contentSection = document.querySelector('.content-section');
-  
-  arrow.addEventListener('click', () => {
-    contentSection.classList.remove('hidden');
-    contentSection.scrollIntoView({ behavior: 'smooth' });
-  });
-});
